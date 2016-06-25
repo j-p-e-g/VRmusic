6de7963030f0a304dcf4f4596b0bcfcada01cc16
Greggman <github@greggman.com>
2016-06-25 18:26:02 +0200

    InstrumentTrigger script

---
+++ Assets/InstrumentTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstrumentTrigger : MonoBehaviour {
+
+    public AudioClip[] audioClips;
+
+    AudioSource m_audioSource;
+
+	// Use this for initialization
+	void Start () {
+        m_audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (audioClips != null && audioClips.Length > 0)
+        {
+            m_audioSource.PlayOneShot(audioClips[0]);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
