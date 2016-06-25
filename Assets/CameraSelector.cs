9596200b81bc9eee9a2995ce325fa67a5da6ec1a
Greggman <github@greggman.com>
2016-06-26 00:24:36 +0900

    initial commit

---
+++ Assets/CameraSelector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSelector : MonoBehaviour {
+
+    public GameObject[] vrGameObjects;
+    public GameObject[] debugGameObjects;
+
+	void Start () {
+        GameObject[] objects = UnityEngine.VR.VRDevice.isPresent ? vrGameObjects : debugGameObjects;
+        foreach(var go in objects)
+        {
+            go.active = true;
+        }
+	}
+	
+}
